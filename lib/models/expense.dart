06c6cff50695567d00a84f3ffcbbb2b4fdff6676
NouranAlSharawneh <noransharawna@hotmail.com>
2024-01-31 19:36:20 +0300

    basic functionality added

---
+++ lib/models/expense.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
+import 'package:intl/intl.dart';
+
+final formatter = DateFormat.yMd();
+const uuid =  Uuid();
+
+// more like types
+enum Category { food, travel, friends, university }
+
+const categoryIcons = {
+  Category.food: Icons.lunch_dining,
+  Category.travel: Icons.flight_takeoff_outlined,
+  Category.friends: Icons.family_restroom_sharp,
+  Category.university: Icons.book_sharp,
+};
+
+class Expense {
+  Expense({
+    required this.title, 
+    required this.amount, 
+    required this.date,
+    required this.category
+  }) : id = uuid.v4();
+
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+  final Category category;
+
+  String get formattedDate {
+    return formatter.format(date);
+  }
+}
---
+++ lib/widgets/new_expense.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class NewExpense extends StatefulWidget {
+  const NewExpense({super.key});
+
+  @override
+  State<NewExpense> createState() => _NewExpenseState();
+}
+
+class _NewExpenseState extends State<NewExpense> {
+  final _titleController = TextEditingController();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _titleController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: [
+          TextField(
+            controller: _titleController,
+            maxLength: 50,
+            keyboardType: TextInputType.text,
+            decoration: const InputDecoration(
+              label: Text('expense name')
+            ),
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  print(_titleController.text);
+                }, 
+                child: const Text('save expense'),
+              )
+            ],
+          )
+        ]
+      ),
+    );
+  }
+}
